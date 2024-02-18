dfb58120b99a335b7c7866d6d80e64a10eecd138
shri123sharma <sshrikant919@gmail.com>
2024-02-19 00:00:11 +0530

    decorator and generator function add

--- python_file/new_file.py
+++ python_file/new_file.py
@@ -1,9 +1,34 @@
-# def func(a:int,b:int):
-#     return a+b
-# result=func(10,20)
-# print(result)
+def func(a:int,b:int):
+    return a+b
+result=func(10,20)
+print(result)
 
 def func1(a:int,b:int):
     return a+b
 result=func1(10,20)
 print(result)
+
+def countdown(n):
+    while n > 0:
+        yield n
+        n -= 1
+
+# Using the generator
+for i in countdown(5):
+    print(i)
+
+# Decorator function
+def my_decorator(func):
+    def wrapper():
+        print("Something is happening before the function is called.")
+        func()  # Call the original function
+        print("Something is happening after the function is called.")
+    return wrapper
+
+# Applying the decorator
+@my_decorator
+def say_hello():
+    print("Hello!")
+
+# Calling the decorated function
+say_hello()
